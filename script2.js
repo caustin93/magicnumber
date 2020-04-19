086de004dadc46fc5e1aa3b27a3c62dab1faecc7
caustin93 <63817822+caustin93@users.noreply.github.com>
2020-04-19 00:55:32 -0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,15 @@
+
+
+secretNo = "6"
+
+var vprompt = prompt("Guess the magic number")
+
+while(vprompt!==secretNo) {
+	if (vprompt<6) {
+		alert("Too low, try again.");
+		var vprompt = prompt("Guess the magic number")
+	} else (alert("Too high, try again"));
+ vprompt = prompt("Guess the magic number")}
+
+ if (vprompt=secretNo){
+		alert("Nice job!")}
